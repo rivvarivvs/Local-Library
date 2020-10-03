0e9e38f4053ddeac1b19882a7835af493e0f80db
rivva <rivvarivvs@gmail.com>
2020-10-03 22:40:46 +0100

    added views and controllers regarding all details

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -1,3 +1,4 @@
+const book = require('../models/book');
 var BookInstance = require('../models/bookinstance');
 
 // Display list of all BookInstances.
@@ -13,8 +14,21 @@ exports.bookinstance_list = function(req, res, next) {
 };
 
 // Display detail page for a specific BookInstance.
-exports.bookinstance_detail = function(req, res) {
-    res.send('NOT IMPLEMENTED: BookInstance detail: ' + req.params.id);
+exports.bookinstance_detail = function(req, res, next) {
+
+    BookInstance
+        .findById(req.params.id)
+        .populate('book')
+        .exec(function(err, bookinstance) {
+            if (err) { return next(err) }
+            if (bookinstance === null) {
+                const err = new Error ('Book copy not found')
+                err.status = 404
+                return next(err)
+            }
+        //Successful, so render
+        res.render('bookinstance_detail', { title: 'Copy: '+bookinstance.book.title,  bookinstance: bookinstance})
+    })
 };
 
 // Display BookInstance create form on GET.
--- controllers/genreController.js
+++ controllers/genreController.js
@@ -1,5 +1,8 @@
 const { nextTick } = require('async');
-var Genre = require('../models/genre');
+const Genre = require('../models/genre');
+const Book = require('../models/book')
+const async = require('async');
+const genre = require('../models/genre');
 
 // Display list of all Genre.
 exports.genre_list = function(req, res) {
@@ -15,9 +18,30 @@ exports.genre_list = function(req, res) {
 };
 
 // Display detail page for a specific Genre.
-exports.genre_detail = function(req, res) {
-    res.send('NOT IMPLEMENTED: Genre detail: ' + req.params.id);
-};
+exports.genre_detail = function(req, res, next) {
+
+    async.parallel({
+        genre: function(callback) {
+            Genre.findById(req.params.id)
+                .exec(callback)
+        },
+
+        genre_books: function(callback) {
+            Book.find({ 'genre': req.params.id })
+                .exec(callback)
+        },
+
+    }, function(err, results) {
+        if (err) { return next(err) }
+        if (results.genre === null) { //No results.
+            const err = new Error('Genre not found')
+            err.status = 404
+            return next(err)
+        }
+        //Succcessful, so render
+        res.render('genre_detail', { title: 'Genre Detail', genre: results.genre, genre_books: results.genre_books })
+    }
+)};
 
 // Display Genre create form on GET.
 exports.genre_create_get = function(req, res) {
